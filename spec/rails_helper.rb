aa493a2348daeb42e968d0730fbea1c9d3b97c66
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-03-20 23:15:01 -0300

    config shoulda_matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :active_record
+      with.library :active_model
+    end
+  end
 end
