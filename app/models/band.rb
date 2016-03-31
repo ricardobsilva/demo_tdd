e3571ef7837e3d4017d650abd3ac66c70f04f096
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-03-30 21:12:02 -0300

    Update model band

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,4 @@
 class Band < ActiveRecord::Base
+  validates :name, :musical_genre , :site, presence:true
+  has_many :musics
 end
--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Band, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:musical_genre) }
+  it { is_expected.to validate_presence_of(:site) }
+  it { is_expected.to have_many(:musics)}
 end
