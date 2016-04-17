6e8cdf8de17ee7733fd93516a40239b0572bc288
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-04-17 10:29:38 -0300

    tests of model band

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -5,4 +5,31 @@ RSpec.describe Band, type: :model do
   it { is_expected.to validate_presence_of(:musical_genre) }
   it { is_expected.to validate_presence_of(:site) }
   it { is_expected.to have_many(:musics)}
+
+  context "is valid" do
+    it 'has a valid factory' do
+      expect(build(:band)).to be_valid
+    end
+  end
+
+  context "is invalid" do
+    it 'without name' do
+      band = build(:band, name: nil)
+      band.valid?
+      expect(band.errors[:name]).to include("can't be blank")
+    end
+
+    it 'without musical_genre' do
+      band = build(:band, musical_genre: nil)
+      band.valid?
+      expect(band.errors[:musical_genre]).to include("can't be blank")
+    end
+
+    it 'without site' do
+      band = build(:band, site: nil)
+      band.valid?
+      expect(band.errors[:site]).to include("can't be blank")
+    end
+  end
+
 end
