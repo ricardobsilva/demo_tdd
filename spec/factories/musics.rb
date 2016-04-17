9ef33f6dbf96453d32458e3a68b90e32a1083052
ricardosilva13 <ricardo.b.silva13@gmail.com>
2016-04-17 10:43:31 -0300

    testes of model music

--- spec/factories/musics.rb
+++ spec/factories/musics.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :music do
-    title "MyString"
-    name_author "MyString"
+    title {Faker::Name.title}
+    name_author {Faker::Name.name}
   end
 end
--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Music, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:name_author) }
+  it { is_expected.to belong_to(:band)}
+
+  context "is valid" do
+    it 'has a valid factory' do
+      expect(build(:music)).to be_valid
+    end
+  end
+
+  context "is invalid" do
+    it 'without title' do
+      music = build(:music, title: nil)
+      music.valid?
+      expect(music.errors[:title]).to include("can't be blank")
+    end
+
+    it 'without name_author' do
+      music = build(:music, name_author: nil)
+      music.valid?
+      expect(music.errors[:name_author]).to include("can't be blank")
+    end
+  end
 end
